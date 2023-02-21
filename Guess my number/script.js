8e96e47aa8e57309c6733626932935e09d245d3a
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-21 14:55:02 +0700

    Add files via upload

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -5,6 +5,10 @@ let score = 20;
 let secretedNumber = Math.ceil(Math.random() * 20 * Math.pow(2, level - 1));
 let highScore = 0;
 let isCorrect = false;
+/* REUSE ELEMENT */
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn-modal");
 /* ********* GENERAL FUNCTION ********* */
 // Get screted number from 1 to n
 let getSecretedNumber = function (n) {
@@ -13,6 +17,27 @@ let getSecretedNumber = function (n) {
 //display message
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
+};
+// open modal window
+const openModal = function () {
+  modal.classList.remove("hidden", "go-to-right");
+  overlay.classList.remove("hidden");
+};
+// close modal window
+const closeModal = function () {
+  modal.classList.add("go-to-right");
+  overlay.classList.add("hidden");
+  // Animation
+  scaleUpDown(".between");
+  scaleUpDown(".label-level");
+  // RESETING
+  score = 20;
+  document.querySelector(".score").textContent = 20;
+  displayMessage(`Start guessing...`);
+  document.querySelector("body").style.backgroundColor = "";
+  document.querySelector(".number").style.width = "";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
 };
 // Scale up and down animation
 const scaleUpDown = function (target) {
@@ -41,20 +66,18 @@ const startTheGame = function () {
     level = 1;
   }
   secretedNumber = Math.ceil(Math.random() * 20 * Math.pow(2, level - 1));
-  console.log(secretedNumber);
-  document.querySelector(".level").textContent = level;
+  document.querySelectorAll(".level").forEach((element) => {
+    element.textContent = level;
+  });
   document.querySelector(".range").textContent = 20 * Math.pow(2, level - 1);
-  // Animation
-  scaleUpDown(".between");
-  scaleUpDown(".label-level");
-  // RESETING
-  score = 20;
-  document.querySelector(".score").textContent = 20;
-  displayMessage(`Start guessing...`);
-  document.querySelector("body").style.backgroundColor = "";
-  document.querySelector(".number").style.width = "";
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").value = "";
+  // Modal window
+  openModal();
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" && !overlay.classList.contains("hidden"))
+      closeModal();
+  });
 };
 /* ********* START THE GAME ********* */
 startTheGame();
